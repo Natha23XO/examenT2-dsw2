db3c114cb65a323765c390d755acd09a6a9d0d85
Natha23XO <hugocastroleon2301@gmail.com>
2024-10-07 19:26:10 -0500

    subiendo a repo

---
+++ src/main/java/pe/edu/cibertec/sw_examen_t2/SwExamenT2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.sw_examen_t2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwExamenT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwExamenT2Application.class, args);
+	}
+
+}
